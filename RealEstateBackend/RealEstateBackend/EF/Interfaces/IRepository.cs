4745cdea2d20bd92636e3d440e2d98ae6592aaf5
João Pedro Basso Audibert <hotsjoao@gmail.com>
2023-09-23 23:15:38 -0300

    Adds Repository interface

---
+++ RealEstateBackend/RealEstateBackend/EF/Interfaces/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace RealEstateBackend.EF.Interfaces
+{
+    public interface IRepository <T>
+    {
+        IEnumerable<T> GetAll();
+        T Get();
+        void Create();
+        void Update();
+        void Delete();
+    }
+}
