fa5c6f0017b17678d1c6c818ac9246e07763153c
João Pedro Basso Audibert <hotsjoao@gmail.com>
2023-09-24 01:56:38 -0300

    Updates interface to return object when updating and creating

--- RealEstateBackend/RealEstateBackend/EF/Interfaces/IRepository.cs
+++ RealEstateBackend/RealEstateBackend/EF/Interfaces/IRepository.cs
@@ -1,13 +1,12 @@
-﻿using RealEstateBackend.RealEstates.Models;
-
-namespace RealEstateBackend.EF.Interfaces
+﻿namespace RealEstateBackend.EF.Interfaces
 {
     public interface IRepository <T>
     {
         Task<IEnumerable<T>> GetAll();
         Task<T> Get(Guid id);
-        Task Create(T type);
-        Task Update(T type, Guid id);
+        Task<T> Create(T type);
+        Task CreateMany(IEnumerable<T> entities);
+        Task<T> Update(T type, Guid id);
         Task Delete(Guid id);
     }
 }
